47a9cb7b6cd622721fbb5233ab464f38ff02d61f
Abraham Lillo <abraham.lillol@gmail.com>
2025-11-07 14:41:53 +0100

    feat(admin): registrar modelos con filtros/búsqueda e inline para detalles

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Categoria, Etiqueta, Producto, DetalleProducto
 
-# Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+    search_fields = ("nombre",)
+
+@admin.register(Etiqueta)
+class EtiquetaAdmin(admin.ModelAdmin):
+    list_display = ("nombre",)
+    search_fields = ("nombre",)
+
+class DetalleInline(admin.StackedInline):
+    model = DetalleProducto
+    can_delete = False
+    extra = 0
+
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "precio", "categoria")
+    list_filter = ("categoria", "etiquetas")
+    search_fields = ("nombre", "descripcion")
+    inlines = [DetalleInline]
